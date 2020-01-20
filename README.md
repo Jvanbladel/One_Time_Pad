14d6ddce54eb410199b3d48cba8dd605c87ec8b0
Jvanbladel <48334749+Jvanbladel@users.noreply.github.com>
2020-01-20 15:17:27 -0800

    Main program
    This is the program that I made in September 2019. I uploaded a readme file that needs completion and the one-tim-pad.py needs work with the key and user experience.

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# one_time_pad
+This project was used to understand how encryptions work with the 
+goal of creating a text-based encryption that would be extremely
+difficult to break without a key. A one-time pad was chosen for the
+project because a one-time pad is considered to be unbreakable if the 
+random key is kept unshared. This being said the encryption is made by 
+taking the local system time and using it as a seed for random number
+generation. This creates a regenerable set of random numbers since it is 
+impossible to create an entirely random number on a computer.
+
+Definition of Encryption:
+In cryptography, encryption is the process of encoding a message 
+or information in such a way that only authorized parties can 
+access it and those who are not authorized cannot. Encryption 
+does not itself prevent interference but denies the intelligible 
+content to a would-be interceptor.
+
+Definition of one-time pad:
+In cryptography, the one-time pad (OTP) is an encryption 
+technique that cannot be cracked, but requires the use of a 
+one-time pre-shared key the same size as, or longer than, the 
+message being sent. In this technique, a plaintext is paired with
+a random secret key (also referred to as a one-time pad).
+
+Instructions for use:
+
+Try to print in command line/Terminal:
+
+pip install pypng
+pip install pillow
+
+To test the “test” image insert the following inputs:
+
+
+If you have any questions feel free to contact me. 
+Thanks!
+Jason Van Bladel
---
+++ one-time-pad.py
@@ -0,0 +1,185 @@
+#Made by Jason Van Bladel
+
+import png
+import random
+from PIL import Image
+from PIL import ImageColor
+import datetime
+import time
+import math
+
+def turnInto256(charInt):
+    if charInt < 69:
+        return charInt + (94*random.randint(0,2))
+    if charInt < 94:
+        return charInt + (94*random.randint(0,1))
+    else:
+        return charInt
+    
+def Two56IntoCharInt(Int):
+    return Int%94
+
+def convertCharacterToASSKEY(char):
+    return ord(char)-32
+
+def convertASSKEYToChar(ASKEY):
+    return chr(ASKEY+32)
+def chooseRandomChar():
+    return random.randint(0,93)
+
+def encryptCharacter(char, key):
+    return ((char + key)%94)
+
+def decyrptCharacter(char, key):
+    output = char - key
+    if output < 0:
+        return 94+output
+    else:
+        return output
+    
+def createKey():
+    #return int(time.time())
+    small = 1
+    for x in range(4096):
+        small *= 2
+    large = 1
+    for x in range(8192):
+        large *= 2
+    value1 = random.randint(small, large)
+    value2 = int(time.time()*100000000000000000000000000000000000000000000000000000000000000000000000000000)
+    
+    return  (value1* value2 + random.randint(1, 9))
+    
+
+def turnListIntoColorList(myList):
+    outputList = list()
+    aList = myList
+    #print(len(aList)%4)
+    while len(aList)%4 != 0:
+        aList.insert(len(aList),256)
+    #print(aList)
+    while len(aList) > 0:
+        outputList.append([aList.pop(0),aList.pop(0),aList.pop(0),aList.pop(0)])
+    return outputList
+
+def createEncrytion(string, imageName):
+    encryptionSize = 255
+    key = createKey()
+    random.seed(key)
+    myList = list()
+    for char in string:
+        #print(convertCharacterToASSKEY(char))
+        rand = chooseRandomChar()
+        #print(rand)
+        #print(encryptCharacter(convertCharacterToASSKEY(char), rand))
+        myList.append(encryptCharacter(convertCharacterToASSKEY(char), rand))
+ 
+    img = Image.new('RGBA', (encryptionSize, encryptionSize), color = 'white')
+
+    colorList = turnListIntoColorList(myList)
+    
+    cordinateList = list()
+    for x in range(encryptionSize):
+        for y in range(encryptionSize):
+            cordinateList.append([x,y])
+            
+    pixelList = list()
+    for pixel in colorList:
+        pixelList.append([cordinateList.pop(random.randint(0, len(cordinateList)-1)),pixel])
+    #print(pixelList)
+    for pixel in pixelList:
+        for x in range(pixel[1].count(256)):
+            pixel[1].remove(256)
+            pixel[1].append(random.randint(0,255))
+
+    for pixel in pixelList:
+        for i in range(len(pixel[1])):
+            item = pixel[1].pop(i)
+            changeValue = turnInto256(item)
+            pixel[1].insert(i, changeValue)
+
+    while len(cordinateList) > 0:
+        newPixel = list()
+        for x in range(4):
+            newPixel.append(random.randint(0,255))
+        pixelList.append([cordinateList.pop(),newPixel])
+
+    pixelMap = img.load()
+    
+    for pixel in pixelList:
+        img.putpixel((pixel[0][0], pixel[0][1]),(pixel[1][0], pixel[1][1], pixel[1][2], pixel[1][3]))
+
+    img.save(imageName)
+    
+    return([key,len(string),encryptionSize])
+    
+def decrypt(filename, key):
+    pixelList = list_of_pixels_in(filename)
+
+    random.seed(key[0])
+    randomList = list()
+    for elem in range(key[1]):
+        randomList.append(chooseRandomChar())
+
+    cordinateList = list()
+    for x in range(key[2]):
+        for y in range(key[2]):
+            cordinateList.append([x,y])
+            
+    pixelOrderList = list()
+    for pixel in range(math.ceil(key[1]/4)):
+        pixelOrderList.append(cordinateList.pop(random.randint(0, len(cordinateList)-1)))
+    
+    myList = list()
+    for elem in pixelOrderList:
+        for i in pixelList:
+            if i[0] == elem:
+                for elem in i[1]:
+                   myList.append(elem)
+    decryptionOutput = ""
+    pos = 0
+    for rand in randomList:
+        letterToDecrypt = Two56IntoCharInt(myList[pos])
+        decryptionOutput = decryptionOutput + convertASSKEYToChar(decyrptCharacter(letterToDecrypt, rand))
+        pos += 1
+        
+    return decryptionOutput[:key[1]]
+
+def list_of_pixels_in(filename):
+    image = Image.open(filename)
+    listOfPixels = list()
+    width, height = image.size
+    
+    for x in range(width):
+        for y in range(height):
+            r, g, b, a = image.getpixel((x,y))
+            listOfPixels.append([[x,y],[r, g, b, a]])
+    return listOfPixels
+def main():
+    while 1:
+        option = input("E or D?" )
+        if option == "E":
+            response = input("Enter Message: ")
+            imageName = input("Enter Image File Name: ")
+            imageName +=".png"
+            key = createEncrytion(response, imageName)
+            print(key)
+        if option == "D":
+            imageName = input("File Name: ")
+            key1 = input("Enter Key: ")
+            key1 = int(key1)
+            key2 = input("Enter Length: ")
+            key2 = int(key2)
+            key3 = input("Size: ")
+            key3 = int(key3)
+            imageName += ".png"
+            print(decrypt(imageName,[key1, key2, key3]))
+    
+    #image = Image.open("hi.png")
+    #print(list_of_pixels_in("hi.png")[0])
+    #print(list_of_pixels_in("hi.png"))
+main()
+
+
+
+
